56c8265202b7a1cc766d5ed8e82bcad183005700
Ayush Srivastava <ayush.srivastava@admiralindia.com>
2026-01-13 04:36:26 +0530

    added a function

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 class HelloWorld{
     public static void main(String[] args) {
+        greet();
         System.out.println("Hello, this is Jenkins CI/CD Pipeline");
+    }
+    public static void greet(){
+        System.out.println("Greetings from Jenkins!");
     }
 }
