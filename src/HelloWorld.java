cc168237cc6ae74b8b7f5af7632ad8268526cc5a
Ayush Srivastava <ayush.srivastava@admiralindia.com>
2026-01-13 05:36:05 +0530

    testing ci pipeline

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("Hello, this is Jenkins CI/CD Pipeline");
+        System.out.println("Hello, this is Jenkins CI Pipeline");
     }
 }
